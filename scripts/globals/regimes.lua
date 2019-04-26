24bfcb4b4dd8a4c092dd0511bd849aeddcbf8017
Chris <hargrave81@hotmail.com>
2019-04-25 23:42:21 -0400

    update dediction

--- scripts/globals/regimes.lua
+++ scripts/globals/regimes.lua
@@ -1050,8 +1050,9 @@ end
 dsp.regime.bookOnEventFinish = function(player, option, regimeType)
     player:addStatusEffect(dsp.effect.DEDICATION,175,0,9600,0,3000)
     player:addStatusEffect(dsp.effect.REGEN, 1, 3, 9600)
-    player:addStatusEffect(dsp.effect.REFRESH, 1, 3, 9600, 0, 3)
-    player:setAnimation(8)
+    player:addStatusEffect(dsp.effect.REFRESH, 1, 3, 9600, 0, 3)    
+    player:addStatusEffect(dsp.effect.ATTACK_BOOST, 18, 0, 9600)
+    --player:setAnimation(8)
     local zoneId = player:getZoneID()
     local msgOffset = zones[zoneId].text.REGIME_REGISTERED
     local tabs = player:getCurrency("valor_point")
