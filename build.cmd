4f1515da6b1feeb85fcd93f51c91a89fd7c2fb0a
Chris <hargrave81@hotmail.com>
2019-05-03 07:27:55 -0400

    add GM Auditing

---
+++ build.cmd
@@ -0,0 +1,8 @@
+cd DevAgent
+Docker build . -t hargrave81/devagent
+cd ..
+docker build -f dockerfile.base -t hargrave81/phoenixbase --no-cache --build-arg CACHEBUST=$(date) .
+docker build . -t hargrave81/phoenix --no-cache
+docker push hargrave81/DevAgent
+docker push hargrave81/phoenixbase
+docker push hargrave81/phoenix
