6c567cc0e5d871f2d5dbb6084805856aa51596a5
hargrave81 <hargrave81@hotmail.com>
2019-05-12 16:40:29 -0400

    survival guide update for mission ranks

--- scripts/zones/Port_Windurst/npcs/Survival_Guide.lua
+++ scripts/zones/Port_Windurst/npcs/Survival_Guide.lua
@@ -1,12 +1,12 @@
 -----------------------------------
--- Area: Bastok Mines
+-- Area: Port Windurst
 --  NPC: Survival Guide
 -- Fast Rank
 -----------------------------------
 require("scripts/globals/keyitems")
 require("scripts/globals/missions")
 require("scripts/globals/settings")
-local ID = require("scripts/zones/Bastok_Mines/IDs")
+local ID = require("scripts/zones/Port_Windurst/IDs")
 -----------------------------------
 
 function onTrade(player,npc,trade)
@@ -14,13 +14,13 @@ function onTrade(player,npc,trade)
 end
 
 function onZoneIn(player,prevZone)
-    player:setVar("BastokMissionFast",0);
+    player:setVar("WindyMissionFast",0);
 end
 
 function onTrigger(player,npc)
-    if player:getVar("BastokMissionFast") == 1 then
-        local logInfo = GetMissionLogInfo(1); -- get where they are currently
-        local areaMissionIds = dsp.mission.id[dsp.mission.area[1]]
+    if player:getVar("WindyMissionFast") == 1 then
+        local logInfo = GetMissionLogInfo(2); -- get where they are currently
+        local areaMissionIds = dsp.mission.id[dsp.mission.area[2]]
         local missionId = 0
         if (missionId ~= nil) then
             missionId = tonumber(missionId) or areaMissionIds[string.upper(missionId)] or _G[string.upper(missionId)];
@@ -31,17 +31,17 @@ function onTrigger(player,npc)
         end
         local i = missionId
         while i < 14 do
-            if i ~= missionId then player:addMission(1, i) end
+            if i ~= missionId then player:addMission(2, i) end
             player:PrintToPlayer("Complete mission #"..i)
-            player:completeMission(1, i)
+            player:completeMission(2, i)
             i = i + 1
         end
         player:setRank(5);
-        player:setVar("BastokMissionFast",0)        
+        player:setVar("WindyMissionFast",0)        
     else
-        if player:getNation() == 1 and player:getRank() < 5 then
+        if player:getNation() == 2 and player:getRank() < 5 then
             player:PrintToPlayer("Talk to me one more time and I'll auto rank you up!")
-            player:setVar("BastokMissionFast",1)
+            player:setVar("WindyMissionFast",1)
         else
             player:PrintToPlayer("You are already so epic!")
         end        
