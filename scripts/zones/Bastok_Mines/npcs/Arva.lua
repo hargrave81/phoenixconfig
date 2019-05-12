a8b55fd249785cd8aae31a3e178397c7e64d9e2d
hargrave81 <hargrave81@hotmail.com>
2019-05-11 21:46:40 -0400

    added new adv. coupon stuff, and sub job fix

---
+++ scripts/zones/Bastok_Mines/npcs/Arva.lua
@@ -0,0 +1,31 @@
+-----------------------------------
+-- Area: Bastok Mines
+--  NPC: Arva
+-- Adventurer's Assistant
+-- Working 100%
+-------------------------------------
+require("scripts/globals/settings");
+local ID = require("scripts/zones/Bastok_Mines/IDs");
+require("scripts/globals/coupon");
+
+function onTrade(player,npc,trade)
+    if (trade:getItemCount() == 1 and trade:hasItemQty(536,1) == true) then
+        player:startEvent(4);
+        player:addGil(GIL_RATE*5000);
+        player:tradeComplete();
+    end
+end;
+
+function onTrigger(player,npc)
+    player:startEvent(3);
+end;
+
+function onEventUpdate(player,csid,option)
+end;
+
+function onEventFinish(player,csid,option)
+    if (csid == 4) then
+        player:messageSpecial(ID.text.GIL_OBTAINED,GIL_RATE*5000);
+        onCoupon(player);
+    end
+end;
---
+++ scripts/zones/Port_Windurst/npcs/Jack_of_Clubs.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Area: Port Windurst
+--  NPC: Jack of Clubs
+-- Adventurer's Assistant
+-- Working 100%
+-------------------------------------
+require("scripts/globals/settings");
+require("scripts/globals/coupon");
+
+function onTrade(player,npc,trade)
+    if (trade:getItemCount() == 1 and trade:hasItemQty(536,1) == true) then
+        player:startEvent(10008,GIL_RATE*5000);
+        player:addGil(GIL_RATE*5000);
+        onCoupon(player);
+        player:tradeComplete();
+    end
+end;
+
+function onTrigger(player,npc)
+    player:startEvent(10007,0,3);
+end;
+
+function onEventUpdate(player,csid,option)
+end;
+
+function onEventFinish(player,csid,option)
+end;
