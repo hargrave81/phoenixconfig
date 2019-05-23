602e9e526cbb320c46e8883c5e3a05a944d9555a
hargrave81 <hargrave81@hotmail.com>
2019-05-23 00:02:48 -0400

    attempt to fix NM slip drops

--- scripts/zones/King_Ramperres_Tomb/mobs/Arcus_Blades.lua
+++ scripts/zones/King_Ramperres_Tomb/mobs/Arcus_Blades.lua
@@ -8,22 +8,30 @@ require("scripts/globals/status");
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
+    player:PrintToPlayer("You sense this monster is good for Thieves, Paladins, Dark Knights, Beast Masters, Samurais and Monks");
+    local ID = zones[player:getZoneID()]    
     if player:getMainJob() == dsp.job.THF and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.ROGUES_ATTIRE_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.ROGUES_ATTIRE_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.PLD and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.GALLANT_ARMOR_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.GALLANT_ARMOR_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.DRK and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.CHAOS_ARMOR_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.CHAOS_ARMOR_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.BST and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.BEAST_ARMOR_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.BEAST_ARMOR_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.MNK and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.TEMPLE_ATTIRE_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.TEMPLE_ATTIRE_CLAIM_SLIP);
     end    
     if player:getMainJob() == dsp.job.SAM and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.MYOCHIN_ARMOR_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.MYOCHIN_ARMOR_CLAIM_SLIP);
     end    
 end
--- scripts/zones/RoMaeve/mobs/Fired_Urn.lua
+++ scripts/zones/RoMaeve/mobs/Fired_Urn.lua
@@ -8,23 +8,31 @@ require("scripts/globals/status");
 
 -----------------------------------
 
-function onMobDeath(mob, player, isKiller)    
+function onMobDeath(mob, player, isKiller)  
+    player:PrintToPlayer("You sense this monster is good for Dragoons, Summoners, Corsairs, Puppet Masters, Dancers and Scholars");
+    local ID = zones[player:getZoneID()]
     if player:getMainJob() == dsp.job.DRG and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.DRACHEN_ARMOR_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.DRACHEN_ARMOR_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.SMN and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.EVOKERS_ATTIRE_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.EVOKERS_ATTIRE_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.COR and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.CORSAIRS_ATTIRE_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.CORSAIRS_ATTIRE_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.PUP and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.PUPPETRY_ATTIRE_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.PUPPETRY_ATTIRE_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.DNC and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.DANCERS_ATTIRE_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.DANCERS_ATTIRE_CLAIM_SLIP);
     end
     if player:getMainJob() == dsp.job.SCH and player:getMainLvl() > 45 then
         player:addKeyItem(dsp.ki.SCHOLARS_ATTIRE_CLAIM_SLIP);
+        player:messageSpecial( ID.text.KEYITEM_OBTAINED, dsp.ki.SCHOLARS_ATTIRE_CLAIM_SLIP);
     end
 end
