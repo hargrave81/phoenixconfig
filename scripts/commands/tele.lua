27d3550234fb4ed25de78874b6046e1a56fa7444
hargrave81 <hargrave81@hotmail.com>
2019-05-24 17:58:44 -0400

    update tele

--- scripts/commands/tele.lua
+++ scripts/commands/tele.lua
@@ -11,16 +11,32 @@ require("scripts/globals/msg")
 cmdprops =
 {
     permission = 0,
-    parameters = "s"
+    parameters = "b"
 };
 
+
+function getBytePos(s,needle)
+    local i;
+    local b;
+    for i=1,string.len(s),1 do
+        if (string.byte(s, i) == needle) then
+            return i;
+        end
+    end
+    return nil;
+end;
 
 function onTrigger(player,destination)    
     local cities = {26,32,48,50,70,71,80,87,94,189,199,214,219,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,256,257,280,284,285};
     local yourZone = player:getZoneID()
     for i, cityId in pairs(cities) do
         if (cityId == yourZone) then
+            local bytes = string.sub(destination,6);
+            local destbyte = getBytePos(destination, 253);
+            local deststr = tostring(bytes);
             player:PrintToPlayer("Attempt to Teleport to Dest > "..destination);
+            player:PrintToPlayer("Attempt to Teleport to Destb > "..destbyte);
+            player:PrintToPlayer("Attempt to Teleport to Dests > "..deststr);
             if destination == "Teleport-Yhoat" or destination == "yhoat" then
                 if (target:hasKeyItem(dsp.ki.YHOATOR_GATE_CRYSTAL) == true) then
                     target:addStatusEffectEx(dsp.effect.TELEPORT,0,dsp.teleport.id.YHOAT,0,4.7)
