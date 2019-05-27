4939877f6448e706688eb1f4c9add3254a48d90b
hargrave81 <hargrave81@hotmail.com>
2019-05-27 09:28:02 -0400

    update sql

--- sql/spiritpact.sql
+++ sql/spiritpact.sql
@@ -1,78 +1,78 @@
-update droplist SET itemid = 4898, itemRate = 10 WHERE dropid = 41 AND itemId = 4106 and itemRate = 150; -- air
-update droplist SET itemid = 4898, itemRate = 5 WHERE dropid = 42 AND itemId = 4106 and itemRate = 150;
-update droplist SET itemid = 4898, itemRate = 5 WHERE dropid = 43 AND itemId = 4106 and itemRate = 150;
+update mob_droplist SET itemid = 4898, itemRate = 10 WHERE dropid = 41 AND itemId = 4106 and itemRate = 150; -- air
+update mob_droplist SET itemid = 4898, itemRate = 5 WHERE dropid = 42 AND itemId = 4106 and itemRate = 150;
+update mob_droplist SET itemid = 4898, itemRate = 5 WHERE dropid = 43 AND itemId = 4106 and itemRate = 150;
 
-update droplist SET itemid = 4903, itemRate = 5 WHERE dropid = 639 AND itemId = 4111 and itemRate = 150; -- dark
-update droplist SET itemid = 4903, itemRate = 8 WHERE dropid = 640 AND itemId = 4111 and itemRate = 150;
-update droplist SET itemid = 4903, itemRate = 10 WHERE dropid = 641 AND itemId = 4111 and itemRate = 150;
-update droplist SET itemid = 4903, itemRate = 10 WHERE dropid = 642 AND itemId = 4111 and itemRate = 150;
+update mob_droplist SET itemid = 4903, itemRate = 5 WHERE dropid = 639 AND itemId = 4111 and itemRate = 150; -- dark
+update mob_droplist SET itemid = 4903, itemRate = 8 WHERE dropid = 640 AND itemId = 4111 and itemRate = 150;
+update mob_droplist SET itemid = 4903, itemRate = 10 WHERE dropid = 641 AND itemId = 4111 and itemRate = 150;
+update mob_droplist SET itemid = 4903, itemRate = 10 WHERE dropid = 642 AND itemId = 4111 and itemRate = 150;
 
-update droplist SET itemid = 4899, itemRate = 10 WHERE dropid = 829 AND itemId = 4107 and itemRate = 150; -- earth
-update droplist SET itemid = 4899, itemRate = 20 WHERE dropid = 830 AND itemId = 4107 and itemRate = 150;
-update droplist SET itemid = 4899, itemRate = 30 WHERE dropid = 831 AND itemId = 4107 and itemRate = 150;
-update droplist SET itemid = 4899, itemRate = 30 WHERE dropid = 832 AND itemId = 4107 and itemRate = 150;
-update droplist SET itemid = 4899, itemRate = 10 WHERE dropid = 833 AND itemId = 4107 and itemRate = 150;
+update mob_droplist SET itemid = 4899, itemRate = 10 WHERE dropid = 829 AND itemId = 4107 and itemRate = 150; -- earth
+update mob_droplist SET itemid = 4899, itemRate = 20 WHERE dropid = 830 AND itemId = 4107 and itemRate = 150;
+update mob_droplist SET itemid = 4899, itemRate = 30 WHERE dropid = 831 AND itemId = 4107 and itemRate = 150;
+update mob_droplist SET itemid = 4899, itemRate = 30 WHERE dropid = 832 AND itemId = 4107 and itemRate = 150;
+update mob_droplist SET itemid = 4899, itemRate = 10 WHERE dropid = 833 AND itemId = 4107 and itemRate = 150;
 
-update droplist SET itemid = 4896, itemRate = 10 WHERE dropid = 955 AND itemId = 4104 and itemRate = 150; -- fire
-update droplist SET itemid = 4896, itemRate = 14 WHERE dropid = 956 AND itemId = 4104 and itemRate = 150;
-update droplist SET itemid = 4896, itemRate = 25 WHERE dropid = 957 AND itemId = 4104 and itemRate = 150;
-update droplist SET itemid = 4896, itemRate = 10 WHERE dropid = 958 AND itemId = 4104 and itemRate = 150;
-update droplist SET itemid = 4896, itemRate = 30 WHERE dropid = 959 AND itemId = 4104 and itemRate = 150;
+update mob_droplist SET itemid = 4896, itemRate = 10 WHERE dropid = 955 AND itemId = 4104 and itemRate = 150; -- fire
+update mob_droplist SET itemid = 4896, itemRate = 14 WHERE dropid = 956 AND itemId = 4104 and itemRate = 150;
+update mob_droplist SET itemid = 4896, itemRate = 25 WHERE dropid = 957 AND itemId = 4104 and itemRate = 150;
+update mob_droplist SET itemid = 4896, itemRate = 10 WHERE dropid = 958 AND itemId = 4104 and itemRate = 150;
+update mob_droplist SET itemid = 4896, itemRate = 30 WHERE dropid = 959 AND itemId = 4104 and itemRate = 150;
 
-update droplist SET itemid = 4897, itemRate = 25 WHERE dropid = 1754 AND itemId = 4105 and itemRate = 150; -- ice
-update droplist SET itemid = 4897, itemRate = 15 WHERE dropid = 1755 AND itemId = 4105 and itemRate = 150;
-update droplist SET itemid = 4897, itemRate = 10 WHERE dropid = 1756 AND itemId = 4105 and itemRate = 150;
-update droplist SET itemid = 4897, itemRate = 25 WHERE dropid = 1757 AND itemId = 4105 and itemRate = 150;
-update droplist SET itemid = 4897, itemRate = 25 WHERE dropid = 1758 AND itemId = 4105 and itemRate = 150;
-update droplist SET itemid = 4897, itemRate = 30 WHERE dropid = 1759 AND itemId = 4105 and itemRate = 150;
+update mob_droplist SET itemid = 4897, itemRate = 25 WHERE dropid = 1754 AND itemId = 4105 and itemRate = 150; -- ice
+update mob_droplist SET itemid = 4897, itemRate = 15 WHERE dropid = 1755 AND itemId = 4105 and itemRate = 150;
+update mob_droplist SET itemid = 4897, itemRate = 10 WHERE dropid = 1756 AND itemId = 4105 and itemRate = 150;
+update mob_droplist SET itemid = 4897, itemRate = 25 WHERE dropid = 1757 AND itemId = 4105 and itemRate = 150;
+update mob_droplist SET itemid = 4897, itemRate = 25 WHERE dropid = 1758 AND itemId = 4105 and itemRate = 150;
+update mob_droplist SET itemid = 4897, itemRate = 30 WHERE dropid = 1759 AND itemId = 4105 and itemRate = 150;
 
-update droplist SET itemid = 4902, itemRate = 10 WHERE dropid = 2059 AND itemId = 4110 and itemRate = 150; -- light
+update mob_droplist SET itemid = 4902, itemRate = 10 WHERE dropid = 2059 AND itemId = 4110 and itemRate = 150; -- light
 
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3483 AND itemId = 4108 and itemRate = 150; -- lightning
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3485 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3486 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3487 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3488 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3489 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3490 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 25 WHERE dropid = 3491 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 25 WHERE dropid = 3492 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 25 WHERE dropid = 3493 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 25 WHERE dropid = 3494 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 15 WHERE dropid = 3495 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3496 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3497 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 15 WHERE dropid = 3500 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3503 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3506 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3507 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3509 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3510 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3512 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3513 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 10 WHERE dropid = 3514 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3516 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3517 AND itemId = 4108 and itemRate = 150;
-update droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3519 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3483 AND itemId = 4108 and itemRate = 150; -- lightning
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3485 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3486 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3487 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3488 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3489 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3490 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 25 WHERE dropid = 3491 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 25 WHERE dropid = 3492 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 25 WHERE dropid = 3493 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 25 WHERE dropid = 3494 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 15 WHERE dropid = 3495 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3496 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3497 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 15 WHERE dropid = 3500 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3503 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3506 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3507 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3509 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3510 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3512 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3513 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 10 WHERE dropid = 3514 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3516 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3517 AND itemId = 4108 and itemRate = 150;
+update mob_droplist SET itemid = 4902, itemRate = 5 WHERE dropid = 3519 AND itemId = 4108 and itemRate = 150;
 
-update droplist SET itemid = 4901, itemRate = 25 WHERE dropid = 4089 AND itemId = 4109 and itemRate = 150; -- water
-update droplist SET itemid = 4901, itemRate = 25 WHERE dropid = 4090 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 15 WHERE dropid = 4091 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4092 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 15 WHERE dropid = 4094 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4095 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4100 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 10 WHERE dropid = 4102 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4104 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4105 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4107 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 20 WHERE dropid = 4109 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4110 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4113 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4114 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 25 WHERE dropid = 4115 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4116 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 15 WHERE dropid = 4117 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4118 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 30 WHERE dropid = 4119 AND itemId = 4109 and itemRate = 150;
-update droplist SET itemid = 4901, itemRate = 30 WHERE dropid = 4120 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 25 WHERE dropid = 4089 AND itemId = 4109 and itemRate = 150; -- water
+update mob_droplist SET itemid = 4901, itemRate = 25 WHERE dropid = 4090 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 15 WHERE dropid = 4091 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4092 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 15 WHERE dropid = 4094 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4095 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4100 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 10 WHERE dropid = 4102 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4104 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4105 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4107 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 20 WHERE dropid = 4109 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4110 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4113 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4114 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 25 WHERE dropid = 4115 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4116 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 15 WHERE dropid = 4117 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 5 WHERE dropid = 4118 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 30 WHERE dropid = 4119 AND itemId = 4109 and itemRate = 150;
+update mob_droplist SET itemid = 4901, itemRate = 30 WHERE dropid = 4120 AND itemId = 4109 and itemRate = 150;
