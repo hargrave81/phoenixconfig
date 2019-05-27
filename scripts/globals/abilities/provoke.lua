fae1b00e453024b5af320af71ddc5a7cde3d849e
hargrave81 <hargrave81@hotmail.com>
2019-05-27 18:02:29 -0400

    a bunch of updates

---
+++ scripts/globals/abilities/provoke.lua
@@ -0,0 +1,16 @@
+-----------------------------------
+-- Ability: Provoke
+-- Goads an enemy into attacking you.
+-- Obtained: Warrior Level 5
+-- Recast Time: 0:30
+-- Duration: 30 seconds
+-----------------------------------
+
+function onAbilityCheck(player,target,ability)
+    return 0,0
+end
+
+function onUseAbility(user,target,ability)
+    --leave blank please! This file will be deleted when the core is updated.
+    target:addEnmity(user, 0, 1000)
+end
