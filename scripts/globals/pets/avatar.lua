dc59617d1ea573eaac32e0b4520cb78cf8033ace
hargrave81 <hargrave81@hotmail.com>
2019-05-27 22:40:09 -0400

    avatar fix

--- scripts/globals/pets/avatar.lua
+++ scripts/globals/pets/avatar.lua
@@ -17,7 +17,7 @@ function onPetEngage(pet,delay)
     local master = pet:getMaster()        
     local mLvl = master:getMainLvl()
     local foodPower = 3 + (mLvl / 10)
-    pet:addStatusEffect(dsp.effect.DEF_BOOST*1.75,foodPower,0,9600,0,0)
+    pet:addStatusEffect(dsp.effect.DEF_BOOST,foodPower*1.75,0,9600,0,0)
     pet:addStatusEffect(dsp.effect.STR_BOOST,foodPower,0,9600,0,0)
     if (pet:getSystem() == 11) then         -- elemental spirit
         pet:addStatusEffect(dsp.effect.INT_BOOST,foodPower/2,0,9600,0,0)
