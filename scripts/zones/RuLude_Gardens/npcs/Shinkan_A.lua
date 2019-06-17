ac5420f616bc412ee0b9e4d40902a3c3271e66fe
hargrave81 <hargrave81@hotmail.com>
2019-06-16 22:04:18 -0400

    update warps

--- scripts/zones/RuLude_Gardens/npcs/Shinkan_A.lua
+++ scripts/zones/RuLude_Gardens/npcs/Shinkan_A.lua
@@ -4,8 +4,8 @@ require("scripts/globals/status")
 function onTrigger(player,npc)
     local talkStatus = player:getVar("ArtifactA")
     if talkStatus == 0 then
-        player:PrintToPlayer("Good evening, I can send you to Castle Z - Where a turtle holding the key to artifact gear exists")
-        player:PrintToPlayer("This turtle holds true for masters of great axes, Singers, archers, mages and throwing masters")        
+        player:PrintToPlayer("Good evening, I can send you to Castle Z - Where The Flame of Fate holding the key to artifact gear exists.")
+        player:PrintToPlayer("This turtle holds true for masters of great axes, Singers, archers, mages and throwing masters.")        
         player:setVar("ArtifactA", 1)
     elseif talkStatus == 1 then
         player:PrintToPlayer("If you'd like to be teleported to the artifact monster, talk to me one more time.")
@@ -13,6 +13,6 @@ function onTrigger(player,npc)
     else
         player:PrintToPlayer("Safe travels")
         player:addStatusEffectEx(dsp.effect.TELEPORT)
-        player:setPos(180.47, -24, -7.63, 0, 161);
+        player:setPos(180.47, -24, -7.63, 180, 161);
     end
 end
--- scripts/zones/RuLude_Gardens/npcs/Shinkan_B.lua
+++ scripts/zones/RuLude_Gardens/npcs/Shinkan_B.lua
@@ -4,8 +4,8 @@ require("scripts/globals/status")
 function onTrigger(player,npc)
     local talkStatus = player:getVar("ArtifactB")
     if talkStatus == 0 then
-        player:PrintToPlayer("Konbanwa, I can send you to the King's Tomb - Where a turtle holding the key to artifact gear exists.")
-        player:PrintToPlayer("This turtle holds true for the most cunning, masters of shields, knights, masters of the animal kingdom, as well as trained martial artists and swordsman")
+        player:PrintToPlayer("Konbanwa, I can send you to the King's Tomb - Where the Arcus Blades is holding the key to artifact gear exists.")
+        player:PrintToPlayer("This turtle holds true for the most cunning, masters of shields, knights, masters of the animal kingdom, as well as trained martial artists and swordsman.")
         player:setVar("ArtifactB", 1)
     elseif talkStatus == 1 then
         player:PrintToPlayer("If you'd like to be teleported to the artifact monster, talk to me one more time.")
@@ -13,6 +13,6 @@ function onTrigger(player,npc)
     else
         player:PrintToPlayer("Safe travels")
         player:addStatusEffectEx(dsp.effect.TELEPORT)
-        player:setPos(24.91, 8.5, 120.2, 0, 190);
+        player:setPos(24.91, 8.5, 120.2, 120, 190);
     end
 end
