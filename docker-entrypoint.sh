f7337474cbd9f5da1a83283de0dba1e9cdc05f76
cjhargrave <cjhargrave@lkqcorp.com>
2020-05-29 23:04:00 -0400

    update version and builds

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,36 +7,36 @@ MYSQL_PORT=${MYSQL_PORT:-3306}
 MYSQL_LOGIN=${MYSQL_LOGIN:-darkstar}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-darkstar}
 MYSQL_DATABASE=${MYSQL_DATABASE:-dspdb}
-SERVERNAME=${SERVERNAME:-DarkStar}
+SERVERNAME=${SERVERNAME:-topaz}
 APP=${APP:-GAME}
-MSG_SERVER=${MSG_SERVER:-172.17.0.1}
+MSG_SERVER=${MSG_SERVER:-10.0.0.24}
 
 ## modify configuration
 function modConfig() {
-    local db_files=(login_darkstar.conf map_darkstar.conf search_server.conf)
+    local db_files=(login.conf map.conf search_server.conf)
 
     for f in ${db_files[@]}
     do
-        if [[ -f /darkstar/conf/$f ]]; then
-            sed -i "s/^\(mysql_host:\s*\).*\$/\1$MYSQL_HOST/" /darkstar/conf/$f
-            sed -i "s/^\(mysql_port:\s*\).*\$/\1$MYSQL_PORT/" /darkstar/conf/$f
-            sed -i "s/^\(mysql_login:\s*\).*\$/\1$MYSQL_LOGIN/" /darkstar/conf/$f
-            sed -i "s/^\(mysql_password:\s*\).*\$/\1$MYSQL_PASSWORD/" /darkstar/conf/$f
-            sed -i "s/^\(mysql_database:\s*\).*\$/\1$MYSQL_DATABASE/" /darkstar/conf/$f            
+        if [[ -f /topaz/conf/$f ]]; then
+            sed -i "s/^\(mysql_host:\s*\).*\$/\1$MYSQL_HOST/" /topaz/conf/$f
+            sed -i "s/^\(mysql_port:\s*\).*\$/\1$MYSQL_PORT/" /topaz/conf/$f
+            sed -i "s/^\(mysql_login:\s*\).*\$/\1$MYSQL_LOGIN/" /topaz/conf/$f
+            sed -i "s/^\(mysql_password:\s*\).*\$/\1$MYSQL_PASSWORD/" /topaz/conf/$f
+            sed -i "s/^\(mysql_database:\s*\).*\$/\1$MYSQL_DATABASE/" /topaz/conf/$f            
         fi
     done
 
-    sed -i "s/^\(servername:\s*\).*\$/\1$SERVERNAME/" /darkstar/conf/login_darkstar.conf
+    sed -i "s/^\(servername:\s*\).*\$/\1$SERVERNAME/" /topaz/conf/login.conf
 }
 
 modConfig
 
 if [ $APP = "LOGIN" ]; then
-  exec /darkstar/dsconnect
+  exec /topaz/topaz_connect
 elif [ $APP = "AUCTION" ]; then 
-  exec /darkstar/dssearch
+  exec /topaz/topaz_search
 elif [ $APP = "GAME" ]; then
-  exec /darkstar/dsgame
+  exec /topaz/topaz_game
 else
   exec /usr/local/bin/supervisord
 fi
