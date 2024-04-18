a44e15270a328670c89c76f1f0632d5b941cc10e
Chris Hargrave <hargrave81@gmail.com>
2024-04-17 21:58:33 -0400

    updates

--- build.cmd
+++ build.cmd
@@ -1,12 +1,13 @@
 cd DevAgent
-Docker build . -t 10.0.0.104:5000/devagent
+Docker build . -t registry.imgdelivered.com/devagent
 cd ..
 cd DBUpdate
-docker build -t 10.0.0.104:5000/phoenixdb . --no-cache
+docker build -t registry.imgdelivered.com/phoenixdb . --no-cache
 cd ..
-docker build -f Dockerfile.base -t 10.0.0.104:5000/phoenixbase --no-cache --build-arg CACHEBUST=$(date) .
-docker build . -t 10.0.0.104:5000/phoenix --no-cache
+docker build -f Dockerfile.base -t registry.imgdelivered.com/phoenixbase --no-cache --build-arg CACHEBUST=$(date) .
+docker build . -t registry.imgdelivered.com/phoenix --no-cache
 
-docker push 10.0.0.104:5000/DevAgent
-docker push 10.0.0.104:5000/phoenixbase
-docker push 10.0.0.104:5000/phoenix
+docker push  registry.imgdelivered.com/devagent
+docker push  registry.imgdelivered.com/phoenixdb
+docker push  registry.imgdelivered.com/phoenixbase
+docker push  registry.imgdelivered.com/phoenix
